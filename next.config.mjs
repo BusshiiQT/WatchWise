ae54ca06535d3da74c25a46526eb7b6cfdd1b52a
Hector Virrey <hv.fin25@gmail.com>
2025-10-07 21:13:24 -0500

    test: add /_check, /api/health, and /__alive.txt

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,5 @@ const nextConfig = {
   images: { remotePatterns: [{ protocol: 'https', hostname: 'image.tmdb.org' }] },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  // IMPORTANT: Do NOT set `output: 'export'`
-  // IMPORTANT: Do NOT set `basePath` unless you intend to serve under a subpath
 };
 export default nextConfig;
